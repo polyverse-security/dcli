22fd6f40b03dcc0eba28e6f59b64f4c037483c2c
Morgan Hein <morgan@polyverse.io>
2019-05-09 13:12:51 -0700

    more basic tests

--- flags/int_test.go
+++ flags/int_test.go
@@ -1,1 +1,18 @@
 package flags
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestNewInt(t *testing.T) {
+	x := NewIntFlag("example", "example int flag", true)
+	y := GetIntFlag("example")
+	assert.Equal(t, x, y)
+}
+
+//this test is meant to test that nil is returned from GetIntFlag() and Value() and doesn't panic
+func TestNilInt(t *testing.T) {
+	y := GetIntFlag("example").Value()
+	assert.Nil(t, y)
+}
